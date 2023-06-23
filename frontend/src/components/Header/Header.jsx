f246fd4bcff2098f5fa48c01ce44306acfe13be8
bibekghosh <bibek7478@gmail.com>
2023-06-23 12:35:56 +0530

    work on menu btn

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import logo from "../../Images/logo.jpg";
 import menuIcon from "../../Images/menu.png";
+import crossIcon from "../../Images/cross.png";
+// import {BiMenu} from 'react-icons/bi';
 
 const Header = () => {
+  const [navBarOpen, setNavBarOpen] = useState(false);
   return (
     <>
       <header class="nav">
@@ -11,7 +14,7 @@ const Header = () => {
           <img src={logo} alt="logo" class="logo" />
         </div>
 
-        <div class="nav-item">
+        <div class={`nav-item ${navBarOpen ? 'show-menu' : ''}`} >
           <ul class="nav-items">
             <a href="/">Home</a>
           </ul>
@@ -25,9 +28,13 @@ const Header = () => {
             <a href="/account">Login</a>
           </button>
         </div>
-        <div class="menu-item">
-          <img src={menuIcon} alt="menu-icon" class="menu-icon" />
-        </div>
+        <button class="menu-item" onClick={()=> setNavBarOpen((prev)=> !prev)}>
+          {navBarOpen? (
+            <img src={crossIcon} alt="menu-icon" class="menu-icon" />
+            ):(
+              <img src={menuIcon} alt="menu-icon" class="menu-icon" />
+          )}
+        </button>
       </header>
     </>
   );
