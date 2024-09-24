a2218636d5b154f0accd8a174c4688061fb912ee
bibekghoshh <bibek7478@gmail.com>
2024-09-24 20:48:30 +0530

    tring again with origin

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,29 @@ import cookieParser from "cookie-parser";
 export const app = express();
 import cors from 'cors';
 
+// app.use(cors({
+//   // origin: 'http://localhost:3000',
+//   origin: 'https://www.bibekghosh.live',
+//   origin: '*',
+//   credentials: true,
+// }));
+
 app.use(cors({
-  // origin: 'http://localhost:3000',
-  origin: 'https://www.bibekghosh.live',
-  origin: '*',
+  origin: (origin, callback) => {
+    const allowedOrigins = ['https://www.bibekghosh.live', 'https://www.bibekghosh.tech'];
+    
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error('Not allowed by CORS')); // Block the request
+    }
+  },
   credentials: true,
 }));
+
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
